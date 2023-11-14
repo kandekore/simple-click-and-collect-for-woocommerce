c15b6337ce459b0c05f761c6d89823a2d3a1c77d
Darren <you@example.comdarren@kandekore.net>
2023-11-14 23:27:26 +0000

    buffer and available time fix

--- js/collection-time-booking.js
+++ js/collection-time-booking.js
@@ -1,58 +1,72 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
   // Initialize datepicker
-
   $("#collection_time_field").hide();
 
-  $('#collection_date').datepicker({
-    dateFormat: 'yy-mm-dd',
+  $("#collection_date").datepicker({
+    dateFormat: "yy-mm-dd",
     minDate: collectionTimeOptions.minDate,
-    beforeShowDay: function(date) {      
+    beforeShowDay: function (date) {
       return [true];
     },
-    onSelect: function(selectedDate) {      
+    onSelect: function (selectedDate) {
+      // Clear existing notices and hide time field initially
+      $("#collection_time_field").hide();
+      $("#times").remove();
 
-      var daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+      var daysOfWeek = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday"
+      ];
+      var selectedDateObj = new Date(selectedDate);
+      var dayOfWeek = daysOfWeek[selectedDateObj.getDay()];
+      var openingHours = collectionTimeOptions.openingHours[dayOfWeek];
+      var html = '<option value="">Select Collection Time</option>';
 
-      var selectedDate1 = new Date(selectedDate);
-      var dayOfWeek = daysOfWeek[selectedDate1.getDay()];
-     
-      var openingHours = collectionTimeOptions.openingHours;
-
-      var curdaytime=openingHours[dayOfWeek];
-
-      // console.log(curdaytime.start_time);
-      // console.log(curdaytime.end_time);
-
-      var html='<option value="">Select Collection Time</option>';
-      
-      if(curdaytime.start_time!='' && curdaytime.end_time!=''){
+      // Generate time slots
+      if (openingHours.start_time != "" && openingHours.end_time != "") {
         $("#collection_time_field").show();
-        $("#collection_time_field > span").show();
-        $("#collection_time_field > #times").remove();
-
-        var st_first=(curdaytime.start_time).split(":");
-        var et_first=(curdaytime.end_time).split(":");
-
-        for(ab=st_first[0];ab<=et_first[0];ab++){
-          html+='<option value="'+ab+': 00">'+ab+': 00</option>';
+        var st_first = openingHours.start_time.split(":");
+        var et_first = openingHours.end_time.split(":");
+        for (
+          var ab = parseInt(st_first[0]);
+          ab <= parseInt(et_first[0]);
+          ab++
+        ) {
+          html += '<option value="' + ab + ': 00">' + ab + ": 00</option>";
         }
-      }if(curdaytime.start_time=='' && curdaytime.end_time==''){
-        $("#collection_time_field").show();
-        $("#collection_time_field > span").hide();
-        $("#collection_time_field").append("<span id='times'>No times available</span>");
+      } else {
+        // Show a notice if no times are available
+        $("#collection_time_field").after(
+          '<div id="times">No times available for the selected date</div>'
+        );
+        return;
       }
 
-      $('#collection_time').html(html);
+      $("#collection_time").html(html);
 
-      if(selectedDate==collectionTimeOptions.curdate){
-        var enddtime=(collectionTimeOptions.minTime).split(":");
-        for(i='10';i<=enddtime[0];i++){
-          $('#collection_time > option[value="'+i+': 00"]').prop('disabled', true);
-        }
-      }else{
-        $('#collection_time > option').prop('disabled', false);
+      // Check if the selected date is the next day
+      var currentDate = new Date();
+      var nextDay = new Date(currentDate);
+      nextDay.setDate(currentDate.getDate() + 1);
+      var isNextDay = selectedDateObj.toDateString() === nextDay.toDateString();
+
+      // Disable past times with a 2-hour buffer
+      if (selectedDate == collectionTimeOptions.curdate || isNextDay) {
+        var bufferHour = isNextDay ? 2 : currentDate.getHours() + 2; // 2-hour buffer from midnight if it's the next day
+        $("#collection_time > option").each(function () {
+          var optionHour = parseInt($(this).val().split(":")[0]);
+          if (optionHour < bufferHour) {
+            $(this).prop("disabled", true);
+          }
+        });
+      } else {
+        $("#collection_time > option").prop("disabled", false);
       }
     }
   });
-
 });
